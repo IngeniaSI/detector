88df7b49b4287635cb4abd0ee60d6514b1891a6f
EduardoReyes <lalo_rait@hotmail.com>
2024-03-20 20:09:12 -0700

    vistas

---
+++ resources/views/Pages/plantilla.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>@yield('tittle')</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="{{ asset('Plantilla/css/styles.css') }}" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="index.html">Detector</a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                <div class="input-group">
+                @if (session()->has('mensaje'))
+                    <script>
+                        alert('{{session("mensaje")}}');
+                    </script>
+                @endif
+                    <!-- <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button> -->
+                </div>
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i> Eduardo Reyes Mtz</a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <!-- <li><a class="dropdown-item" href="#!">Settings</a></li>
+                        <li><a class="dropdown-item" href="#!">Activity Log</a></li> -->
+                        <li><hr class="dropdown-divider" /></li>
+                        <li>
+                        <form action="{{route('logout')}}" method="post">
+                            @csrf
+                            <button class="dropdown-item" >Cerrar sesion</button>
+                        </form>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Menú</div>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-bar"></i></div>
+                                Estadística
+                            </a>
+                            <a class="nav-link" href="encuesta.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-file"></i></div>
+                                Encuesta
+                            </a>
+                            <a class="nav-link" href="tablaEncuesta.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Tabla de Encuestas
+                            </a>
+                            <a class="nav-link" href="mapa.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-map"></i></div>
+                                Mapa Geoespacial
+                            </a>
+                            
+                            <a class="nav-link" href="usuarios.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                                Usuarios
+                            </a>
+                            <a class="nav-link" href="bitacora.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-info"></i></div>
+                                Bitácora
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Usuario:</div>
+                        Administrador
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    @yield('cuerpo')
+                </main>
+
+            </div>    
+        </div>   
+         
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('Plantilla/js/scripts.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.bootstrap4.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.colVis.min.js"></script>
+        <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+        <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap4.min.js"></script>
+        @yield('scripts')        
+    </body>
+   
+	
+
+    
+</html>
--- resources/views/inicioSesion.blade.php
+++ resources/views/inicioSesion.blade.php
@@ -7,16 +7,84 @@
     <title>Login</title>
 </head>
 <body>
-    <form action="{{route('login.comprobando')}}" method="post">
-        @csrf
-        <label for="">Correo</label>
-        <input type="email" name="correo" id="correo" minlength="3" maxlength="255">
-        <label for="">Contraseña</label>
-        <input type="password" name="contrasenia" id="contrasenia" minlength="3" maxlength="255">
-        <button>Iniciar sesion</button>
-        @error('email')
-            <h4>{{$message}}</h4>
-        @enderror
-    </form>
+   
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Detector</title>
+        <link href="{{ asset('Plantilla/css/styles.css') }}" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <style>
+        body, html {
+    height: 100%;
+    background-repeat: no-repeat;
+    background: url({{ asset('Plantilla/assets/img/fondo.jpg') }}) no-repeat center center fixed;
+    background-size: 100% 100%;
+}
+
+    </style>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Detector</h3></div>
+                                    <div class="card-body">
+                                    <form action="{{route('login.comprobando')}}" method="post" class="form-floating mb-3">
+                                        @csrf
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" type="email" name="correo" id="correo" min="3"  />
+                                            <label for="inputEmail">Correo Electronico</label>
+                                        </div>
+                                        
+
+                                        <div class="form-floating mb-3">
+                                                <input class="form-control" type="password" name="contrasenia" id="contrasenia"  />
+                                                <label for="inputPassword">Contraseña</label>
+                                        </div>
+                                        
+                                        
+                                        <center><button class="btn btn-primary">Iniciar sesion</button></center>
+                                        @error('email')
+                                            <h4>{{$message}}</h4>
+                                        @enderror
+                                    </form>
+                                        
+                                    </div>
+                                    <!-- <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+                                    </div> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; IngeniaIS 2024</div>
+                            
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('Plantilla/js/scripts.js') }}"></script>
+    </body>
+</html>
+
+
