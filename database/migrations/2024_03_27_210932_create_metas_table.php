b5607f81ae2a4b48341861fe61df63a2b55dfe05
CarlosEmilio <62856339+FBGKenko@users.noreply.github.com>
2024-03-27 15:00:54 -0700

    tabla de meta

--- database/migrations/2024_03_27_210932_create_metas_table.php
+++ database/migrations/2024_03_27_210932_create_metas_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('metas', function (Blueprint $table) {
             $table->id();
             $table->integer('numeroObjetivo');
+            $table->integer('poblacionEstablecida');
             $table->timestamps();
         });
     }
