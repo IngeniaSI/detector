28af9229eacc364a57030ed51f8048bfd2284dee
CarlosEmilio <EmilioMendozA20@hotmail.com>
2024-05-24 19:58:28 -0700

    nuevas tablas de marketing

--- app/Models/bitacora.php
+++ app/Models/bitacora.php
@@ -2,13 +2,32 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class bitacora extends Model
 {
     use HasFactory;
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    public static function crearRegistro($accion, $url, $ip, $tipo, $idUser){
+        try{
+            DB::beginTransaction();
+            $bitacora = new bitacora();
+            $bitacora->accion = $accion;
+            $bitacora->url = $url;
+            $bitacora->ip = $ip;
+            $bitacora->tipo = $tipo;
+            $bitacora->user_id = $idUser;
+            $bitacora->save();
+            DB::commit();
+        }
+        catch (Exception $e){
+            DB::rollBack();
+        }
     }
 }
---
+++ database/migrations/2024_05_22_193830_create_seguimientos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seguimientos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('oportunidad_id')->constrained();
+            $table->foreignId('catalogo_seguimiento_id')->constrained();
+            $table->date('fecha_registro');
+            $table->time('hora_registro');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seguimientos');
+    }
+};
